092076abaf81fbf936184e604f6f8ee268c12fe9
Seth Todd <seth.todd@outlook.com>
2020-01-21 03:10:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 counter = 0
 def turn_count(board)
   counter = 0
@@ -15,3 +16,14 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
+=======
+board = ["X", " ", " " "O", " ", "X"]
+counter = 0
+def turn_count(board)
+  board.each do |turn|
+    if "#{turn}" == "X"
+      counter += 1
+    end
+  end
+end
+>>>>>>> 2c1953d6c25eacb76b12a9c0a437a4bfb09e6841
